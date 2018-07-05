1ae23a166692310a11b389fcc70509b9c6c6beb6
jwcorle <jcorley.w@gmail.com>
2018-07-05 15:50:52 -0400

    Expanded the selection.

--- cage.js
+++ cage.js
@@ -1,1 +1,1 @@
-[].slice.call(document.getElementsByTagName("img")).forEach(e=>e.src="https://jwcorle.github.io/caging/"+Math.floor(Math.random()*6)+".jpg")
+[].slice.call(document.getElementsByTagName("img")).forEach(e=>e.src="https://jwcorle.github.io/caging/"+Math.floor(Math.random()*11)+".jpg")
